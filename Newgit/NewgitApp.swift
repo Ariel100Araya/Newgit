c4471ab851b2688292c1cbba103c40f10b9adf49
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2025-12-06 12:17:13 -0500

    Initial Commit

---
+++ Newgit/NewgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewgitApp.swift
+//  Newgit
+//
+//  Created by Ariel Araya-Madrigal on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NewgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
